f97e389713d2f09864cf6c5553ed6fabe8031355
jaewook <ljo9287@naver.com>
2021-04-29 23:47:10 +0900

    week9.py

---
+++ week9.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+x = int(input())
+y = 3*x*x + 5*x + 3
+print(y)
+
+
+# In[ ]:
+
+
+r = float(input())
+pi = 3.14
+circum = 2 * pi * r
+area = pi * r * r
+print (circum, area)
+
+
+# In[ ]:
+
+
+
+
